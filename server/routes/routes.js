345254d11e291964528e4f5aa4b2f238d4ad437d
Luke Wilson <wilson.palooza@gmail.com>
2017-02-14 07:59:03 -0800

    add PUT route to update student record

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -5,8 +5,10 @@ var studentController = require('../students/studentController.js');
 module.exports = function (app, express) {
   app.get('/api/students', studentController.getStudents);
   app.post('/api/students', studentController.newStudent);
+  app.put('/api/students', studentController.updateStudent);
 
   app.post('/api/preferences', studentController.studentPrefs);
+
 
 
   //front end helpers
