631cb14183748067828be184c0159f0c86a07aa1
Luke Wilson <wilson.palooza@gmail.com>
2017-02-13 17:55:04 -0800

    redirect routes to controller module

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,13 @@
+var studentController = require('../students/studentController.js');
+
+
+
 module.exports = function (app, express) {
   app.get('/api/test', (req, res) => {
     console.log('get request received');
     res.send('got it again');
-  })
+  });
+
+  //create a new student
+  app.post('/api/students', studentController.newStudent);
 };
