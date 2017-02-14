c3a781a5c8a6d4d47193f2d5acdd07b6771aee5b
Luke Wilson <wilson.palooza@gmail.com>
2017-02-14 07:59:59 -0800

    add updateStudent function and reconfigure newStudent function to include workwith and noworkwith

--- server/students/studentController.js
+++ server/students/studentController.js
@@ -5,7 +5,9 @@ module.exports = {
   newStudent: (req, res) => {
     var newStudent = Student.build({
       firstname: req.body.firstname,
-      lastname: req.body.lastname
+      lastname: req.body.lastname,
+      workwith: req.body.workwith,
+      noworkwith: req.body.noworkwith
     });
     newStudent.save();
     res.send('new student created');
@@ -14,6 +16,18 @@ module.exports = {
   //create student preferences
   studentPrefs: (req, res) => {
     res.send('supposed to create preferences here');
+  },
+
+  updateStudent: (req, res) => {
+    Student.findById(req.body.id)
+    .then(student => {
+      student.firstname = req.body.firstname;
+      student.lastname = req.body.lastname;
+      student.workwith = req.body.workwith;
+      student.noworkwith = req.body.noworkwith;
+      student.save();
+      res.send(`Student updated: ${student.firstname} ${student.lastname}`);
+    });
   },
 
   //get all students
