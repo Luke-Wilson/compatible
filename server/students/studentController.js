5273cdbe1095c25087f85e3bb45ebdae9ffbe774
Luke Wilson <wilson.palooza@gmail.com>
2017-02-13 22:40:56 -0800

    create student DB requests

--- server/students/studentController.js
+++ server/students/studentController.js
@@ -1,9 +1,37 @@
 var Student = require('./studentModel.js');
 
 module.exports = {
+  //create a new student
   newStudent: (req, res) => {
-    console.log('from studentController.js');
-    res.send('got it from studentController.js');
+    var newStudent = Student.build({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname
+    });
+    newStudent.save();
+    res.send('new student created');
+  },
+
+  //create student preferences
+  studentPrefs: (req, res) => {
+    res.send('supposed to create preferences here');
+  },
+
+  //get all students
+  getStudents: (req, res) => {
+    Student.findAll({})
+    .then( students => {
+      res.send(students);
+    });
+  },
+
+  //get a student record from an id number
+  getStudentById: (req, res) => {
+    Student.find({
+      id: req.params.id
+    })
+    .then( student => {
+      res.send(student);
+    });
   }
 
 };
