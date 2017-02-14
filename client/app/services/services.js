1484e90c66892e0fa8c1cfec383886666c3fe5a9
Luke Wilson <wilson.palooza@gmail.com>
2017-02-13 16:29:01 -0800

    add basic angular app structure

---
+++ client/app/services/services.js
@@ -0,0 +1,6 @@
+angular.module('compatible.services', [])
+.factory('Students', function($http) {
+  return {
+    //http request functions to server go in here.
+  };
+});
---
+++ client/app/students/students.js
@@ -0,0 +1,4 @@
+angular.module('compatible.students', [])
+.controller('StudentsController', function($scope) {
+  $scope.test = "This is a test from the Students Controller";
+});
