bc12961dc8031031602188455aef1570493c6b17
Luke Wilson <wilson.palooza@gmail.com>
2017-02-14 11:04:05 -0800

    add and configure basic ui router

--- client/app/app.js
+++ client/app/app.js
@@ -1,4 +1,23 @@
 angular.module('compatible', [
   'compatible.students',
-  'compatible.services'
-  ]);
+  'compatible.services',
+  'ui.router'
+  ])
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // $urlRouterProvider.otherwise('/home');
+  // $urlRouterProvider
+
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '<div>hello?</div>'
+    })
+
+    .state('students', {
+      url: '/students',
+      templateUrl: '/app/students/students.html'
+    });
+
+});
