0d5df124ed84d32a23098f731fd8d796038e6f8b
Luke Wilson <wilson.palooza@gmail.com>
2017-02-14 08:00:22 -0800

    add workwith and noworkwith to student model ;

--- server/students/studentModel.js
+++ server/students/studentModel.js
@@ -4,7 +4,9 @@ var db = require('../db/db.js');
 
 var Student = db.define('student', {
   firstname: Sequelize.STRING,
-  lastname: Sequelize.STRING
+  lastname: Sequelize.STRING,
+  workwith: Sequelize.ARRAY(Sequelize.INTEGER),
+  noworkwith: Sequelize.ARRAY(Sequelize.INTEGER)
 });
 
 module.exports = Student;
