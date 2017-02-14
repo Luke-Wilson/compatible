b8f02393e65c4411ed494b73bf5dbff55c6228e0
Luke Wilson <wilson.palooza@gmail.com>
2017-02-13 22:40:25 -0800

    change model to lowercase so postgres will create table name without quotes

--- server/students/studentModel.js
+++ server/students/studentModel.js
@@ -2,7 +2,7 @@ var Sequelize = require('sequelize');
 var db = require('../db/db.js');
 
 
-var Student = db.define('Student', {
+var Student = db.define('student', {
   firstname: Sequelize.STRING,
   lastname: Sequelize.STRING
 });
