21ef4f2a4ce9b8531167845a38934b347f3d54f6
Luke Wilson <wilson.palooza@gmail.com>
2017-02-16 07:30:39 -0800

    add getStudents function to StudentsController for basic functionality testing

--- client/app/students/students.js
+++ client/app/students/students.js
@@ -1,4 +1,14 @@
 angular.module('compatible.students', [])
-.controller('StudentsController', function($scope) {
+.controller('StudentsController', function($scope, Students) {
   $scope.test = "This is a test from the Students Controller";
+
+  $scope.list = [1,1];
+
+  $scope.getStudents = function() {
+    Students.getStudents()
+    .then(result => {
+      console.log(result);
+      $scope.list = result;
+    });
+  }
 });
