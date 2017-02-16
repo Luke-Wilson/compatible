84f45ef55be770dd28834d97802d75f49f251b2f
Luke Wilson <wilson.palooza@gmail.com>
2017-02-16 07:29:40 -0800

    add getStudents function to students factory

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,6 +1,15 @@
 angular.module('compatible.services', [])
 .factory('Students', function($http) {
   return {
+    getStudents: function() {
+      return $http ({
+        method: 'GET',
+        url: '/api/students'
+      }).then(students => {
+        // console.log(students);
+        return students.data;
+      });
+    }
     //http request functions to server go in here.
   };
 });
